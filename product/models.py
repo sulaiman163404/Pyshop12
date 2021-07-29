36c1563089586403a6f18a011692718f6198cb62
sulaiman163404 <sulaiman.kadyrov@gmail.com>
2021-07-29 15:57:15 +0600

    Add permision, filtration, search, pagination.

--- product/models.py
+++ product/models.py
@@ -32,3 +32,6 @@ class Product(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('detail', kwargs={'id': self.pk})
+
+    class Meta:
+        ordering = ['-id']
--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x$+@sk_%o#%gmj64(8@)&v*@h!k%=b*wz0wivkj^%ax!647aum'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pyshop_db',
-        'USER': 'hello',
-        'PASSWORD': '1',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': 5432
     }
